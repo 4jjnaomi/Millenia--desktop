1fadfae9b212bb099297509e2db1363d5a993085
4jjnaomi <115657585+4jjnaomi@users.noreply.github.com>
2022-11-30 17:13:13 +0000

    Initial

---
+++ java/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    document.getElementById("stormzy").addEventListener('click', function() {
+        document.getElementById("stormzy").location.assign="../html/1a.stormzy_new_vid.html"
+    }, false)
+
+})
